d0544c9d062358d69f813b64f5671d8128c43e0c
david-castaneda <dcast188@fiu.edu>
2018-12-15 19:22:51 -0500

    added react-router

---
+++ src/components/Register.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Register extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <form>
+          <input placeholder="" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Register;
